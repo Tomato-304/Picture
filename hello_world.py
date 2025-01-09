cba3fdda04f4673745a873e6317dd1a6523f3f79
Tomato-304 <yyc629817@163.com>
2025-01-09 13:46:40 +0800

    Delete rubbish

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+name="ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
